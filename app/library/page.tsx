58048bf0e8a68acabad010ebc5012a7a327a61f6
24samj <csumit961@gmail.com>
2025-05-16 20:20:14 +0530

    add library page

---
+++ app/library/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const LibraryPage = (props: Props) => {
+  return (
+    <div className="bg-brown-50 text-brown-75 grid min-h-full place-items-center text-2xl font-semibold tracking-wider">
+      Coming Soon!
+    </div>
+  );
+};
+
+export default LibraryPage;
